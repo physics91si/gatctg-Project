ddac4a8460aa08a2b035a964ab02cbaebb487668
Alec Lourenco <aleclourenco@Alecs-MacBook-Pro.local>
2017-05-28 23:51:36 -0700

    wordcloud display is done; however am getting a weird bug since I cannot connect to Pubmed more than once

---
+++ milestone6/wordCloud.py
@@ -0,0 +1,26 @@
+from os import path
+from wordcloud import WordCloud
+from scraper import *
+
+array = runPubMed('drew', "endy", '+synthetic+biology')
+
+text = ""
+for words in array:
+    text += words
+
+# Generate a word cloud image                                                                                                                                                                               
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:                                                                                                                                                                              
+# the matplotlib way:                                                                                                                                                                                       
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size                                                                                                                                                                                       
+#wordcloud = WordCloud(max_font_size=40).generate(text)
+#plt.figure()
+#plt.imshow(wordcloud, interpolation="bilinear")
+#plt.axis("off")
+#xsplt.show()
+
